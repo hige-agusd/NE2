8f8b295f80949f8d36bd09a5e3d690df0dc7ec82
hige-agusd <diazagustin@gmail.com>
2017-10-19 12:13:36 -0300

    resultados con dist

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,32 +14,32 @@ angular.module('escrutinioApp')
      $scope.listas = [];
      $scope.resultados = [];
 
-     SessionSrv.login({name: 'pabloTest', password: 'test'}).then(function(response){
-       ListasSrv.getListas().then(function(listas) {
-         $scope.listas = listas;
-         console.log(listas)
+     //SessionSrv.login({name: 'pabloTest', password: 'test'}).then(function(response){
+       //ListasSrv.getListas().then(function(listas) {
+         //$scope.listas = listas;
+         //console.log(listas)
          ResultadosSrv.getResultados().then(function(resultados) {
            $scope.resultados = resultados;
            console.log(resultados)
            mostrarResultados();
          });
-       });
-     });
+       //});
+     //});
 
      $scope.calcColor = function(longColor) {
        var r = longColor%256,
            g = parseInt(longColor / 256) % 256,
            b = parseInt(longColor / 256 / 256) % 256,
            retVal = '#';
-           retVal += (r) ? r.toString(16) : '00';
-           retVal += (g) ? g.toString(16) : '00';
            retVal += (b) ? b.toString(16) : '00';
+           retVal += (g) ? g.toString(16) : '00';
+           retVal += (r) ? r.toString(16) : '00';
            console.log(retVal, r,g,b, longColor)
        return retVal;
      };
 
      function mostrarResultados() {
-       console.log($scope.listas, $scope.resultados);
+       //console.log($scope.listas, $scope.resultados);
      }
 
   }]);
